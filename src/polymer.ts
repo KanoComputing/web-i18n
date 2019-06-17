5b38e6023f51c2d614d2934f0219678af26565f8
Paul Varache <paul@kano.me>
2019-06-17 15:17:26 +0100

    Fix types

--- src/polymer.ts
+++ src/polymer.ts
@@ -5,7 +5,7 @@ type Constructor<T> = new(...args: any[]) => T;
 /**
  * A Polymer mixin adding the localize method to use in templates
  */
-export function I18nMixin<T extends Constructor<HTMLElement>>(base : T) {
+export function I18nMixin<T extends Constructor<any>>(base : T) {
     return class extends base {
         localize(key : string, fallback? : string) {
             return _(key, fallback);
